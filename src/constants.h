b5cc9df2e8582372278d3f42b6931f8a4ee7e0dc
Mark Devenyi <markdevenyidev@gmail.com>
2025-03-24 18:21:52 -0400

    refactor: extract constants into `constants.h`

---
+++ src/constants.h
@@ -0,0 +1,11 @@
+#include <math.h>
+
+#define is_known(x) (x >= 0)
+
+#define NUM_SPECIES  3
+#define NUM_ROWS     3
+#define CELL_WIDTH   60
+#define CELL_HEIGHT  20
+#define TABLE_X      50
+#define TABLE_Y      40
+#define ENTER_LINE_Y 230
--- src/main.c
+++ src/main.c
@@ -2,14 +2,7 @@
 
 #include "statusbar.h"
 #include "input.h"
-
-#define NUM_SPECIES  3
-#define NUM_ROWS     3
-#define CELL_WIDTH   60
-#define CELL_HEIGHT  20
-#define TABLE_X      50
-#define TABLE_Y      40
-#define ENTER_LINE_Y 230
+#include "constants.h"
 
 const char *species[NUM_SPECIES] = { "A", "B", "C" };
 const char *rows[NUM_ROWS] = { "Coeff", "Initial", "Final" };
