bbf8283dcdf1ce880a02145d4f95598d87eb28a4
Mark Devenyi <markdevenyidev@gmail.com>
2025-03-25 09:29:33 -0400

    feat: add `equations.h` header

---
+++ src/equations.h
@@ -0,0 +1,20 @@
+#include <math.h>
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MIN3(a, b, c) (MIN(a, MIN(b, c)))
+
+inline float calc_pKa(float K_value) {
+    return -log10f(K_value);
+}
+
+inline float calc_henderson_hasselbach(float K_value, float A, float HA) {
+    return calc_pKa(K_value) + log10f((A / HA));
+}
+
+inline float calc_pH(float H_plus) {
+    return -log10f(H_plus);
+}
+
+inline float calc_pOH_from_pH(float pH) {
+    return 14 - pH;
+}
